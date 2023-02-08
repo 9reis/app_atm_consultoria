cc15ac0d8b59a70ad85e4c06944746d7145f25f7
Lucas Reis <lucasreis_9@hotmail.com>
2023-02-07 21:47:42 -0300

    Enviando email

--- app/src/main/java/br/com/lucas9reis/atmconsultoria/MainActivity.java
+++ app/src/main/java/br/com/lucas9reis/atmconsultoria/MainActivity.java
@@ -1,5 +1,7 @@
 package br.com.lucas9reis.atmconsultoria;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -28,12 +30,12 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //FloatingActionButton
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                enviarEmail();
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
@@ -49,6 +51,27 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    public void enviarEmail( ){
+        /*
+        String celular = "989928284";
+        String img = "https://www.rbsdirect.com.br/imagesrc/24093638.jpg?w=700";
+        Intent intent = new Intent( Intent.ACTION_DIAL, Uri.parse(celular)); // Intenção
+        Intent intentImg = new Intent( Intent.ACTION_VIEW, Uri.parse(img)); // Utilizado para abrir o google Maps
+        startActivity(intentImg); // Executa a intenção
+         */
+
+        Intent intentShare = new Intent( Intent.ACTION_SEND ); // Compartilhar algo
+        intentShare.putExtra( Intent.EXTRA_EMAIL, new String[]{"teste@teste.com"});  //Param direto na intent // Destinatario
+        intentShare.putExtra( Intent.EXTRA_SUBJECT, "Assunto do email");
+        intentShare.putExtra( Intent.EXTRA_TEXT, "Msg do email");
+        intentShare.setType("message/rfc822");// )Tipo de dado compartilhado (email)
+        //Mime Types
+        //text.plain - Texto padrão
+        //image/* - Qualquer tipo de imagem
+
+        startActivity(Intent.createChooser(intentShare, "Compartilhar")); // seta os apps que podem corresponder ao tipo de dado
     }
 
     @Override
